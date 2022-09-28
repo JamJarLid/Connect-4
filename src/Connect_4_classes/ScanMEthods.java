f7f7930a05167e3f4cc2bebe8b30ae35d384b189
Espen Lever <eko15ele@student.lu.se>
2022-09-28 13:17:06 +0200

    Changed scan methods

--- src/Connect_4_classes/ScanMEthods.java
+++ src/Connect_4_classes/ScanMEthods.java
@@ -1,72 +1,66 @@
 package Connect_4_classes;
 
+import static Connect_4_classes.Main.board;
+
 public class ScanMEthods {
 
-    public boolean scanVertical(char playCheck, int streak){
-        boolean[] streaks = new boolean[streak];
+    public int scanVertical(char playCheck, int streak){
+        boolean streaks = true;
         for (int row = board.length - 1; row >= board.length - 3; row--) {
             for (int col = 0; col < board[0].length; col++) {
                 for (int i = 0; i < streak; i++){
-                    streaks[i] = board[row - i][col] == playCheck;
+                    streaks = streaks && board[row - i][col] == playCheck;
+                }
+                if(streaks){
+                    return col;
                 }
             }
         }
-        for (boolean what: streaks) {
-            if(what == false){
-                return false;
-            }
-        }
-        return true;
+        return -1;
     }
 
-    public boolean scanHorizontal(char playCheck, int streak){
-        boolean[] streaks = new boolean[streak];
+    public int scanHorizontal(char playCheck, int streak){
+        boolean streaks = true;
         for (int row = board.length - 1; row >= 0; row--) {
             for (int col = 0; col < board[0].length - 3; col++) {
                 for (int i = 0; i < streak; i++){
-                    streaks[i] = board[row][col + i] == playCheck;
+                    streaks = streaks && board[row][col + i] == playCheck;
+                }
+                if(streaks){
+                    return col;
                 }
             }
         }
-        for (boolean what: streaks) {
-            if(what == false){
-                return false;
-            }
-        }
-        return true;
+        return -1;
     }
 
-    public boolean scanDiagonal1(char playCheck, int streak){
-        boolean[] streaks = new boolean[streak];
+    public int scanDiagonal1(char playCheck, int streak){
+        boolean streaks = true;
         for (int row = (board.length - 1); row >= board.length - 3; row--) {
             for (int col = board[0].length - 1; col >= board[0].length - 4; col--) {
                 for (int i = 0; i < streak; i++){
-                    streaks[i] = board[row - i][col - i] == playCheck;
+                    streaks = streaks && board[row - i][col - i] == playCheck;
+                }
+                if(streaks){
+                    return col;
                 }
             }
         }
-        for (boolean what: streaks) {
-            if(what == false){
-                return false;
-            }
-        }
-        return true;
+        return -1;
     }
 
-    public boolean scanDiagonal2(char playCheck, int streak){
-        boolean[] streaks = new boolean[streak];
+    public int scanDiagonal2(char playCheck, int streak){
+        boolean streaks = true;
         for (int row = board.length - 1; row >= 3 ; row--) {
             for (int col = 0; col < board[0].length - 3; col++) {
                 for (int i = 0; i < streak; i++){
-                    streaks[i] = board[row - i][col + i] == playCheck;
+                    streaks = board[row - i][col + i] == playCheck;
+                }
+                if(streaks){
+                    return col;
                 }
             }
         }
-        for (boolean what: streaks) {
-            if(what == false){
-                return false;
-            }
-        }
-        return true;
+        return -1;
     }
 }
